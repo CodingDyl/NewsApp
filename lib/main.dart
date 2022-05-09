13e400262599fce7499111a1b3d1fbd624be5b54
DylanPetzer <2610dylan@gmail.com>
2022-05-09 15:53:19 +0200

    Starting files

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/views/home.dart';
+
+//api key cd87473495ed4fd585bbaccf6d8376b2
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: Home(),
+    );
+  }
+}
